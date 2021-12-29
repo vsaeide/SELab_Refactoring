3ff77fe9f712577cb5f83a83cd6a8617747cd04e
vsaeide <vsaeide7798@gmail.com>
2021-12-29 14:13:38 +0330

    add default to switch case

--- src/main/java/codegenerator/CodeGenerator.java
+++ src/main/java/codegenerator/CodeGenerator.java
@@ -132,6 +132,9 @@ public class CodeGenerator {
             case 33:
                 defMain();
                 break;
+            default:
+                ErrorHandler.printError("invalid semantic function number");
+                break;
         }
     }
 
