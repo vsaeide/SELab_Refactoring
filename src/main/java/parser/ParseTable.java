9900f6640ba64c5f77da0e1e3c972a596509e90a
vsaeide <vsaeide7798@gmail.com>
2021-12-29 22:22:27 +0330

    edit local variable names

--- src/main/java/parser/ParseTable.java
+++ src/main/java/parser/ParseTable.java
@@ -8,52 +8,47 @@ import java.util.*;
  * Created by mohammad hosein on 6/25/2015.
  */
 public class ParseTable {
-    private List<Map<Token,Action>> actionTable;
-    private List<Map<NonTerminal,Integer>> gotoTable;
+    private List<Map<Token, Action>> actionTable;
+    private List<Map<NonTerminal, Integer>> gotoTable;
+
     public ParseTable(String jsonTable) throws Exception {
-        jsonTable = jsonTable.substring(2,jsonTable.length()-2);
-        String[] Rows = jsonTable.split("\\],\\[");
+        jsonTable = jsonTable.substring(2, jsonTable.length() - 2);
+        String[] rows = jsonTable.split("\\],\\[");
         Map<Integer, Token> terminals = new HashMap<Integer, Token>();
-        Map<Integer,NonTerminal> nonTerminals = new HashMap<Integer, NonTerminal>();
-        Rows[0] = Rows[0].substring(1,Rows[0].length()-1);
-        String[] cols = Rows[0].split("\",\"");
+        Map<Integer, NonTerminal> nonTerminals = new HashMap<Integer, NonTerminal>();
+        rows[0] = rows[0].substring(1, rows[0].length() - 1);
+        String[] cols = rows[0].split("\",\"");
         for (int i = 1; i < cols.length; i++) {
-            if(cols[i].startsWith("Goto")){
+            if (cols[i].startsWith("Goto")) {
                 String temp = cols[i].substring(5);
                 try {
                     nonTerminals.put(i, NonTerminal.valueOf(temp));
-                }catch (Exception ignored){
+                } catch (Exception ignored) {
                 }
-            }
-            else {
+            } else {
                 terminals.put(i, new Token(Token.getTyepFormString(cols[i]), cols[i]));
             }
         }
         actionTable = new ArrayList<Map<Token, Action>>();
         gotoTable = new ArrayList<Map<NonTerminal, Integer>>();
-        for (int i = 1; i <Rows.length ; i++) {
+        for (int i = 1; i < rows.length; i++) {
 
-            Rows[i] = Rows[i].substring(1,Rows[i].length()-1);
-            cols = Rows[i].split("\",\"");
+            rows[i] = rows[i].substring(1, rows[i].length() - 1);
+            cols = rows[i].split("\",\"");
             actionTable.add(new HashMap<Token, Action>());
             gotoTable.add(new HashMap<>());
-            for (int j = 1; j <cols.length ; j++) {
-                if(!cols[j].equals(""))
-                {
-                    if(cols[j].equals("acc")){
-                        actionTable.get(actionTable.size()-1).put(terminals.get(j),new Action(Act.ACCEPT,0));
-                    }else
-                    if(terminals.containsKey(j))
-                    {
+            for (int j = 1; j < cols.length; j++) {
+                if (!cols[j].equals("")) {
+                    if (cols[j].equals("acc")) {
+                        actionTable.get(actionTable.size() - 1).put(terminals.get(j), new Action(Act.ACCEPT, 0));
+                    } else if (terminals.containsKey(j)) {
                         Token t = terminals.get(j);
                         Action a = new Action(cols[j].charAt(0) == 'r' ? Act.REDUCE : Act.SHIFT, Integer.parseInt(cols[j].substring(1)));
-                            actionTable.get(actionTable.size() - 1).put(t, a);
+                        actionTable.get(actionTable.size() - 1).put(t, a);
 
-                    }
-                    else if(nonTerminals.containsKey(j)){
-                        gotoTable.get(gotoTable.size()-1).put(nonTerminals.get(j),Integer.parseInt(cols[j]));
-                    }
-                    else {
+                    } else if (nonTerminals.containsKey(j)) {
+                        gotoTable.get(gotoTable.size() - 1).put(nonTerminals.get(j), Integer.parseInt(cols[j]));
+                    } else {
                         throw new Exception();
                     }
                 }
@@ -61,12 +56,11 @@ public class ParseTable {
         }
     }
 
-    public int getGotoTable(int currentState, NonTerminal variable )
-    {
-            return gotoTable.get(currentState).get(variable);
+    public int getGotoTable(int currentState, NonTerminal variable) {
+        return gotoTable.get(currentState).get(variable);
     }
-    public Action getActionTable(int currentState ,Token terminal)
-    {
+
+    public Action getActionTable(int currentState, Token terminal) {
         return actionTable.get(currentState).get(terminal);
     }
 
--- src/main/java/parser/Rule.java
+++ src/main/java/parser/Rule.java
@@ -1,5 +1,7 @@
 package parser;
+
 import scanner.token.Token;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,8 +13,8 @@ public class Rule {
         int index = stringRule.indexOf("#");
         if (index != -1) {
             try {
-            semanticAction = Integer.parseInt(stringRule.substring(index + 1));
-            }catch (NumberFormatException ex){
+                semanticAction = Integer.parseInt(stringRule.substring(index + 1));
+            } catch (NumberFormatException ex) {
                 semanticAction = 0;
             }
             stringRule = stringRule.substring(0, index);
@@ -20,35 +22,36 @@ public class Rule {
             semanticAction = 0;
         }
         String[] splited = stringRule.split("->");
-            LHS = NonTerminal.valueOf(splited[0]);
+        LHS = NonTerminal.valueOf(splited[0]);
         RHS = new ArrayList<GrammarSymbol>();
         if (splited.length > 1) {
-            String[] RHSs = splited[1].split(" ");
-            for (String s : RHSs){
+            String[] rhss = splited[1].split(" ");
+            for (String s : rhss) {
                 try {
                     RHS.add(new GrammarSymbol(NonTerminal.valueOf(s)));
                 } catch (Exception e) {
-                        RHS.add(new GrammarSymbol(new Token(Token.getTyepFormString(s), s)));
+                    RHS.add(new GrammarSymbol(new Token(Token.getTyepFormString(s), s)));
                 }
             }
         }
     }
+
     public NonTerminal LHS;
     public List<GrammarSymbol> RHS;
     public int semanticAction;
 }
 
-class GrammarSymbol{
+class GrammarSymbol {
     public boolean isTerminal;
     public NonTerminal nonTerminal;
     public Token terminal;
-    public GrammarSymbol(NonTerminal nonTerminal)
-    {
+
+    public GrammarSymbol(NonTerminal nonTerminal) {
         this.nonTerminal = nonTerminal;
         isTerminal = false;
     }
-    public GrammarSymbol(Token terminal)
-    {
+
+    public GrammarSymbol(Token terminal) {
         this.terminal = terminal;
         isTerminal = true;
     }
